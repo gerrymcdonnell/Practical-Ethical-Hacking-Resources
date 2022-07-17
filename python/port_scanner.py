c0983f75b0cf7cfa5e15f0951b27b3d71bd404e0
Gerry Mc Donnell <gerrymcdonnell@users.noreply.github.com>
2022-07-17 17:07:50 +0100

    Update port_scanner.py
    now scans ranges:
    port_scanner.py localhost 1-1000

--- python/port_scanner.py
+++ python/port_scanner.py
@@ -2,28 +2,37 @@ import sys
 from datetime import datetime as dt
 import socket
 
-if len(sys.argv) == 2:
+from rx import catch
+
+if len(sys.argv) == 3:
+  target = socket.gethostbyname(sys.argv[1]) # translates host to ipv4
+  #get port range from cmd line
+  port_range=sys.argv[2].split('-')
+  start_port=int(port_range[0])
+  end_port=int(port_range[1])
+elif len(sys.argv) == 2:
+  start_port=1
+  end_port=1000
   target = socket.gethostbyname(sys.argv[1]) # translates host to ipv4
 else:
   print("Invalid number of args")
-  print("Syntax: python3 port_scanner.py [ip/hostname]")
+  print("Syntax: python3 port_scanner.py [ip/hostname] [port: start-end eg 1-80]")
 
 print("Scannning target: " + target)
 print("Time started: " + str(dt.now()))
 print('-' * 50)
 
 
-def scan_all(start,end):
-    for port in range(start,end):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(0.25)
-        result = s.connect_ex((target, port))
-        if result == 0:
-            print("port {} is open".format(port))
-        s.close 
-
 try:
-    scan_all(1,80)
+  print("scanning ports: {} to {}".format(start_port,end_port))
+  for port in range(start_port,end_port):
+    #print("scanning port {}".format(port))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(0.5)
+    result = s.connect_ex((target, port))
+    if result == 0:
+        print("port {} is open".format(port))
+    s.close 
 except KeyboardInterrupt:
   print('\nExitting...')
   sys.exit()
@@ -33,4 +42,3 @@ except socket.gaierror:
 except socket.error:
   print("Couldn't connect to server")
   sys.exit()
-
